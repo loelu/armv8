5f460015ba78221ae05fc1ff5e226d67117f05d6
laquesisa <dana.shmaria@gmail.com>
2017-12-27 00:02:31 +0100

    SOS works on external LED

--- armv7_act_led/source/gpio.s
+++ armv7_act_led/source/gpio.s
@@ -1,10 +1,10 @@
 .globl GetGpioAddress
-GetGpioAddress:
+GetGpioAddress: // return in r0 the gpio address
 	ldr r0,=0x3F200000
 	mov pc,lr // write adress of next instruction into the pc
 
 .globl SetGpioFunction
-	SetGpioFunction: // paramater r0 which gpio pin and r1 which function
+SetGpioFunction: // paramater r0 which gpio pin and r1 which function
 	pinNum .req r0 // set alias
 	pinFunc .req r1
 	cmp pinNum,#53 // check if input is correct, so pinNum <= 53 and pinFunc <= 7
@@ -27,17 +27,18 @@ GetGpioAddress:
 	add pinNum, pinNum,lsl #1 // multiplication pinNum x 3
 	lsl pinFunc,pinNum
 	.unreq pinNum
-	str pinFunc,[gpioAddr] // problem, that all other gpio pins in the register block go back to 0
+	str pinFunc,[gpioAddr]
 	.unreq pinFunc
 	.unreq gpioAddr
 	pop {pc} // copy latest stack value into pc, same as mov pc,lr
 
 .globl SetGpio
-SetGpio:
+SetGpio: // parameter r0 which gpio pin and r1 which function (turn led off or on)
 	pinNum .req r0
 	pinVal .req r1
 
 	cmp pinNum,#53
+	cmpls pinVal,#1
 	movhi pc,lr
 	push {lr}
 	mov r2,pinNum
--- armv7_act_led/source/main.s
+++ armv7_act_led/source/main.s
@@ -9,37 +9,44 @@ main:
 	mov sp,#0x8000 // move the stackpointer
 	pinNum .req r0
 	pinFunc .req r1
-	mov pinNum,#47
+	mov pinNum,#17
 	mov pinFunc,#1
 	bl SetGpioFunction // set gpio pin as an output
 	.unreq pinNum
 	.unreq pinFunc
+	
+	ptrn .req r4
+	ldr ptrn,=sos
+	ldr ptrn,[ptrn]
+	seq .req r5
+	mov seq,#0
 
 	loop$:
 		pinNum .req r0
+		mov pinNum,#17
 		pinVal .req r1
-		mov pinNum,#47
-		mov pinVal,#0
-		bl SetGpio // turn led on
-		.unreq pinNum
-		.unreq pinVal
-
-		delayDur .req r0
-		ldr delayDur,=100000
-		bl Wait // delay for time = delayDuration
-		.unreq delayDur
-
-		pinNum .req r0
-		pinVal .req r1
-		mov pinNum,#47
 		mov pinVal,#1
-		bl SetGpio // turn led off
+		lsl pinVal,seq
+		and pinVal,ptrn
+		lsr pinVal,seq
+		bl SetGpio // turn led on or off
 		.unreq pinNum
 		.unreq pinVal
 
 		delayDur .req r0
-		ldr delayDur,=100000
-		bl Wait // delay for time = delayDuration
+		ldr delayDur,=250000
+		bl Wait // delay for time = delayDuration, here 0.25 sec
 		.unreq delayDur
-
+		
+		add seq,#1 // pointer of the pattern goes one up
+		and seq,#0b11111 // if seq is greater than 31 it will set back to 0
+		
 		b loop$ // endless loop
+
+
+.section .data
+.align 2
+sos:
+	.int 0b11111110101011000100010001101010
+cat:
+	.int 0b11111111100011000101101000101000
